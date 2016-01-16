0d27f807598fa0166e0d93481f1d1aa5749dfc02
Kristian Lyubenov <kristian.lubenov@gmail.com>
2016-01-16 13:09:51 +0200

    Refactoring

---
+++ TaskManagment/TaskManagment.Data/TMDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using TaskManagment.Models;
+
+namespace TaskManagment.Data
+{
+    public class TMDbContext : IdentityDbContext<User>
+    {
+        public TMDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TMDbContext Create()
+        {
+            return new TMDbContext();
+        }
+    }
+}
--- TaskManagment/TaskManagment.Services/App_Start/Startup.Auth.cs
+++ TaskManagment/TaskManagment.Services/App_Start/Startup.Auth.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
+using TaskManagment.Data;
 using TaskManagment.Services.Providers;
-using TaskManagment.Services.Models;
 
 namespace TaskManagment.Services
 {
@@ -23,7 +19,7 @@ namespace TaskManagment.Services
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(TMDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
